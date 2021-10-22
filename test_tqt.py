ae7b926c6dadd80931f1e7f5fb36384bf5694e83
gimolai <38439709+gimolai@users.noreply.github.com>
2021-10-22 16:19:30 +0000

    Update test_tqt.py

--- test_tqt.py
+++ test_tqt.py
@@ -6,14 +6,14 @@ import unittest
 
 def run(cmd):
   p = subprocess.run(cmd, shell=True, capture_output=True)
-  return (p.returncode, p.stdout.decode('utf-8').rstrip(), p.stderr.decode('utf-8').rstrip())
+  return (p.returncode, p.stdout.decode("utf-8").rstrip(), p.stderr.decode("utf-8").rstrip())
 
 
-class TestSafeSh(unittest.TestCase):
+class TestTQT(unittest.TestCase):
   def setUp(self):
-    self.cmd = "./safesh.py"
-    self.testfile = "./testfile_safesh.txt"
-    self.testfile_py = "./testfile_safesh.py"
+    self.cmd = "./tqt.py"
+    self.testfile = "./testfile_tqt.txt"
+    self.testfile_py = "./testfile_tqt.py"
     self.testfile_text = r"""# comment
 # lines
 "
@@ -46,50 +46,66 @@ print(f"{d.year}/{d.month}/{d.day}")
   def test_blank(self):
     rc, _, err = run(f"{self.cmd}")
     self.assertEqual(rc, 2)
-    self.assertRegex(err, r'error: the following arguments are required: command')
+    self.assertRegex(err, r"error: the following arguments are required: command")
 
   def test_help(self):
     rc, out, _ = run(f"{self.cmd} -h")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'usage: safesh\.py .[-]h. .[-]f FILE. .[-]e EXPRESSION')
+    self.assertRegex(out, r"usage: tqt\.py .[-]h. .[-]V. .[-]f FILE .FILE \.\.\...")
 
   def test_wrong_command(self):
     rc, _, err = run(f"{self.cmd} undefined")
-    self.assertEqual(rc, 1)
-    self.assertRegex(err, r'SyntaxError: command to be executed has to be <cat\|grep\|sed>')
+    self.assertEqual(rc, 2)
+    self.assertRegex(err, r"error: argument command: invalid choice: 'undefined' \(choose from 'cat', 'cut', 'grep', 'sed'\)")
 
   def test_wrong_argument(self):
     rc, _, err = run(f"{self.cmd} cat {self.testfile}")
     self.assertEqual(rc, 2)
-    self.assertRegex(err, r'error: unrecognized arguments:')
+    self.assertRegex(err, r"error: unrecognized arguments:")
 
   def test_cat_stdin(self):
     rc, out, _ = run(f"{self.cmd} cat < {self.testfile}")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'Hello\n,\nhelloooo,\nWorld')
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
+
+  def test_cat_pipe(self):
+    rc, out, _ = run(f"ls | {self.cmd} cat")
+    self.assertEqual(rc, 0)
+    self.assertRegex(out, r"tqt\.py")
 
   def test_cat_f(self):
     rc, out, _ = run(f"{self.cmd} cat -f {self.testfile}")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'Hello\n,\nhelloooo,\nWorld')
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
 
   def test_cat_file(self):
     rc, out, _ = run(f"{self.cmd} cat --file {self.testfile}")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'Hello\n,\nhelloooo,\nWorld')
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
+
+  def test_cat_files(self):
+    rc, out, _ = run(f"{self.cmd} cat --file {self.testfile} {self.testfile_py}")
+    self.assertEqual(rc, 0)
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
+    self.assertRegex(out, r"#!/usr/bin/env python3\nfrom datetime import date")
 
   def test_cat_ff(self):
     rc, out, _ = run(f"{self.cmd} cat -f {self.testfile} -f {self.testfile_py}")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'Hello\n,\nhelloooo,\nWorld')
-    self.assertRegex(out, r'#!/usr/bin/env python3\nfrom datetime import date')
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
+    self.assertRegex(out, r"#!/usr/bin/env python3\nfrom datetime import date")
 
   def test_cat_file_file(self):
     rc, out, _ = run(f"{self.cmd} cat --file {self.testfile} --file {self.testfile_py}")
     self.assertEqual(rc, 0)
-    self.assertRegex(out, r'Hello\n,\nhelloooo,\nWorld')
-    self.assertRegex(out, r'#!/usr/bin/env python3\nfrom datetime import date')
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
+    self.assertRegex(out, r"#!/usr/bin/env python3\nfrom datetime import date")
+
+  def test_grep_file(self):
+    rc, out, _ = run(f"{self.cmd} grep -f {self.testfile} -e '.*'")
+    self.assertEqual(rc, 0)
+    self.assertRegex(out, r"Hello\n,\nhelloooo,\nWorld")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   unittest.main(verbosity=2)
